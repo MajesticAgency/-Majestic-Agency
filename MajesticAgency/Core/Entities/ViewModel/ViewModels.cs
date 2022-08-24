512eb62ed3a9a10b2e76b234b8984d9cf84b553a
Ola M AL-Shlool <olashlool94@gmail.com>
2022-08-24 16:13:55 +0300

    Majestic Agency

---
+++ MajesticAgency/Core/Entities/ViewModel/ViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.ViewModel
+{
+    public class ViewModels
+    {
+        public List<Service> Services { get; set; }
+        public List<SubService> SubServices { get; set; }
+    }
+}
---
+++ MajesticAgency/MajesticAgency/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Core.Entities.ViewModel;
+using Core.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MajesticAgency.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IService _service;
+        private readonly ISubService _subService;
+
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IService service, ISubService subService)
+        {
+            _logger = logger;
+            _service = service;
+            _subService = subService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewModels viewModels = new ViewModels();
+            viewModels.Services = await _service.GetServices();
+            return View(viewModels);
+        }
+
+        public async Task<IActionResult> ServiceDetails(int id)
+        {
+            var service = await _service.GetService(id);
+            return View(service);
+        }
+
+    }
+}
