74ee175195f572e65e06daec867b3b32818eadf7
Ola M AL-Shlool <olashlool94@gmail.com>
2022-09-01 03:27:02 +0300

    Update Career

---
+++ MajesticAgency/MajesticAgency/wwwroot/assets/js/test.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#example-includeSelectAllOption').multiselect({
+            includeSelectAllOption: true
+        });
+    });
+</script>
